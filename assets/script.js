42fa86e232cb4d9817f93af912dbd720f74bc827
Rosana Moraes <rosanasmoraes@yahoo.com>
2022-02-16 01:28:12 -0600

    Changes according to Jonathon video recording on Tuesday Feb15th - assessment review  -  Rosana's js script fie is red not orange - it did not displayed the alerts on screen - except for the first one - needs to be fixed

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+window.alert("Hello from Script");
+//How many items do we have//
+//let store the date for how many items//
+let totalQuantity =1
+
+//reference the text//
+let quantityText = document.querySelector (".total-quantity")
+
+// reference buttons//
+let upButton = document.querySelector ("#quantity-up")
+let downButton = document.querySelector ("quantity-down")
+
+//register listeners for when buttons are clicked//
+
+upButton.addEventlistener ("click"), function (){
+   window.alert ("up button clickes")
+   textQuantity = totalQuantity +1 
+   quantityText.textDocument="Quantity:" = totalQuantity
+}
+downButton.addEventListener ("click"), function (){
+      window.alert ("down buton ")
+     
+      //if zero we do not want to go negative//
+
+} if (totalQuantity > 0 ) {
+   totalQuantity = totalQuantity -1
+}
+
+ }
+
+
